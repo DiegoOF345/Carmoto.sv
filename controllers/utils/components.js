869192737f120b93358bfbdd3e2291c577378efd
Paniagua1234 <20220100@ricaldone.edu.sv>
2024-06-12 08:20:17 -0600

    a

--- controllers/utils/components.js
+++ controllers/utils/components.js
@@ -2,7 +2,7 @@
 *   CONTROLADOR DE USO GENERAL EN TODAS LAS PÁGINAS WEB.
 */
 // Constante para establecer la ruta base del servidor.
-const SERVER_URL = 'http://localhost/Carmoto.sv/CARSMOTOSV/CARSMOTOSV/api/';
+const SERVER_URL = 'http://localhost/Carmoto.sv/api/';
 
 /*
 *   Función para mostrar un mensaje de confirmación. Requiere la librería sweetalert para funcionar.
